231ff5cbbb6e666847e5d5db8bd6ca08fb67cf07
s-santoro <ssantoro@ntb.ch>
2018-12-08 19:43:11 +0100

    added initial cache for images

--- public/sw.js
+++ public/sw.js
@@ -1,51 +1,54 @@
-let cacheName = 'todo_v1';
+let cacheName = "todo_v1";
 
 /**
  * ------- SERVICEWORKER MUSS IN PUBLIC-PFAD SEIN, SONST KANN ER NICHT ÜBER ALLE DATEIEN ARBEITEN
  */
-self.addEventListener('install', (event) => {
-  console.log('installing');
+self.addEventListener("install", event => {
+  console.log("installing");
   event.waitUntil(
     caches
       .open(cacheName)
-      .then((cache) => {
+      .then(cache => {
         return cache.addAll([
-          './manifest.json',
-          './javascript/script.js',
-          './stylesheets/style.css',
-          './images/favicon.ico',
+          "./manifest.json",
+          "./javascript/script.js",
+          "./stylesheets/style.css",
+          "./images/favicon.ico",
+          "./images/icon_192.png",
+          "./images/icon_512.png",
+          "./index.html"
         ]);
       })
-      .catch((error) => console.log('Error while installing', error))
+      .catch(error => console.log("Error while installing", error))
   );
-  console.log('installed');
+  console.log("installed");
 });
 
 /**
  * Intercepting network-request to check cache before fetching.
  */
-self.addEventListener('fetch', (event) => {
-  console.log('fetching');
+self.addEventListener("fetch", event => {
+  console.log("fetching");
   event.respondWith(
     caches
       .match(event.request)
-      .then((response) => {
+      .then(response => {
         if (response) return response;
         // not in cache => request and save it in cache
         let requestClone = event.request;
-        fetch(requestClone).then((res) => {
+        fetch(requestClone).then(res => {
           if (!res || res.status !== 200) {
             return res;
           }
           let resClone = res.clone();
-          caches.open(cacheName).then((cache) => {
+          caches.open(cacheName).then(cache => {
             cache.put(request, resClone);
           });
           return res;
         });
       })
-      .catch((error) => {
-        console.log('Failed to fetch: ', error);
+      .catch(error => {
+        console.log("Failed to fetch: ", error);
       })
   );
 });
