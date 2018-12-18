987723f2f5db796c653063da2b49abb0d541e801
s-santoro <ssantoro@ntb.ch>
2018-12-18 18:26:55 +0100

    trying to resolve problem with cdn-content caching 1

--- public/sw.js
+++ public/sw.js
@@ -18,11 +18,7 @@ self.addEventListener('install', (event) => {
           './images/favicon.ico',
           './images/icon_192.png',
           './images/icon_512.png',
-          './index.html',
-          'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
-          'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js',
-          'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js',
-          'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js'
+          './index.html'
         ]);
       })
       .catch((error) => console.log('Error while installing', error))
