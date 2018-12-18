be1f0f6a7b610e49714d0ca8a38ec36d5bc2f74b
s-santoro <ssantoro@ntb.ch>
2018-12-18 18:12:36 +0100

    initial cache also contains bootstrap.css/js, jquery, popperjs

--- public/sw.js
+++ public/sw.js
@@ -19,6 +19,10 @@ self.addEventListener('install', (event) => {
           './images/icon_192.png',
           './images/icon_512.png',
           './index.html',
+          'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+          'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js',
+          'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js',
+          'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js',
         ]);
       })
       .catch((error) => console.log('Error while installing', error))
